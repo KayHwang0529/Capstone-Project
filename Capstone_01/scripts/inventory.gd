0b0a15768020161b7fe07cd859493eb72e1478ac
B-Varner <105959703+B-Varner@users.noreply.github.com>
2025-05-14 23:46:16 -0400

    CollectItemStep checks new items

--- Capstone_01/scripts/inventory.gd
+++ Capstone_01/scripts/inventory.gd
@@ -2,11 +2,13 @@ class_name Inv extends Resource
 
 @export var items: Array[ItemEntity]
 
-signal update 
+signal item_inserted(item)
+signal update
 
 func insert (item: ItemEntity):
 	items[0] = item
 	print(items)
+	item_inserted.emit(item)
 	
 func contains (item: ItemEntity):
 	if items[0] == item:
--- Capstone_01/scripts/queststeps/collect_item_step.gd
+++ Capstone_01/scripts/queststeps/collect_item_step.gd
@@ -6,12 +6,19 @@ var inv: Inv = preload("res://playerInv.tres")
 
 
 func ready() -> void:
+	inv.item_inserted.connect(new_inv_item)
 	is_collected()
+
+
 
 func is_collected():
 	collected = inv.contains(item)
 	print(inv.items[0])
 	if collected:
+		emit_signal("updated")
+
+func new_inv_item(new_item):
+	if new_item == item:
 		emit_signal("updated")
 
 	
